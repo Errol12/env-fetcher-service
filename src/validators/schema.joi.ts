ff3d737a28f16949c9cad99788cff4b452d80ee1
Errol12 <errolf07@gmail.com>
2025-06-30 16:28:55 +0530

    updated keys limit to 50 and ncu

--- src/validators/schema.joi.ts
+++ src/validators/schema.joi.ts
@@ -21,7 +21,7 @@ export const MetadataSchema = Joi.object({
   keys: Joi.alternatives().conditional('path', {
     is: Joi.exist(),
     then: Joi.forbidden(), // If path is present, keys must not be sent
-    otherwise: Joi.array().items(Joi.string()).max(10).optional(),
+    otherwise: Joi.array().items(Joi.string()).max(50).optional(),
   }),
 });
 
