dc99a2c177480c0f7d6803f3a3bb38bfd901a68e
Errol12 <errolf07@gmail.com>
2026-07-14 16:01:29 +0530

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -22,7 +22,7 @@ describe('App', () => {
   describe('version', () => {
     it('should return the package version', () => {
       const version = app.version();
-      expect(version).toBe('2.0.1');
+      expect(version).toBe('2.0.0');
     });
   });
 
